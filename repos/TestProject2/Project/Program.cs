73cfc4aeea96e21d368f90f2247a03b24852b019
DreamFXX <filipek.husak@gmail.com>
2024-11-02 17:08:15 +0100

    Methods learning from MS

---
+++ repos/TestProject2/Project/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Metody - MS Learn");
+
+
+void displayRandomNumbers() 
+{
+Random random = new Random();
+
+for(int i = 0; i < 5; i++)
+{
+    Console.Write($"{random.Next(1, 100)} ");
+}
+
+}
