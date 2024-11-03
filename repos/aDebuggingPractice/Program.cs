db56f4716097d00aeb38dbe50f8c4a4ca03f4219
DreamFXX <filipek.husak@gmail.com>
2024-11-04 00:49:52 +0100

    merge + pulled deletions of useless repos

--- repos/aDebuggingPractice/Program.cs
+++ repos/aDebuggingPractice/Program.cs
@@ -1,18 +1,28 @@
-﻿namespace aDebuggingPractice
+namespace aDebuggingPractice
 {
 
     public class VocalCounter
     {
+        public static List<char> _refererenceVowels;
 
         public static void Vowels(string word) // Česky samohlásky
+        { // Put a breakpoint with F9, F10 bere krok po kroku kroky porgramu, 
+            // var vowels = new List<char> {'a', 'e', 'i', 'o', 'u'};
 
-        {   // Put a breakpoint with F9, F10 bere krok po kroku kroky porgramu, 
-            var vowels = new List<char> {'a', 'e', 'i', 'o', 'u'};
+            var numberOfVowels = 0;
 
-            var result = word.ToLower().Count(x => vowels.Contains(x));
+            foreach (var letter in _refererenceVowels) //´najetím kurzoru na jmeno variable - zobrazeni hodnoty variable.
+            {
+                foreach (var character in word)
+                {
+                    if (letter == character)
+                    {
+                        numberOfVowels++;
+                    }
+                }
+            }
 
-
-            Console.WriteLine($"Number of vowels in {word}: {result}.");
+            Console.WriteLine($"Number of vowels in {word}: {numberOfVowels}");
         }
     }
 
@@ -20,7 +30,7 @@
     {
         static void Main(string[] args)
         {
-            VocalCounter.Vowels("PIKACHU"); // watermelon
+            VocalCounter.Vowels("PIKACHU");
 
 
             Console.ReadKey();
