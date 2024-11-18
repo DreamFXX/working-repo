37e852c2d166fd9ada21db9f925bd251a1d9382d
DreamFXX <filipek.husak@gmail.com>
2024-11-18 20:57:48 +0100

    Movie model created

--- !Blazor/BlazorWebAppMovies/Models/Movie.cs
+++ !Blazor/BlazorWebAppMovies/Models/Movie.cs
@@ -1,6 +1,19 @@
-﻿namespace BlazorWebAppMovies.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorWebAppMovies.Models;
+
+public class Movie
 {
-    public class Movie
-    {
-    }
+    public int Id { get; set; }
+
+    public string? Title { get; set; } // ? - can hold null value
+
+    public DateOnly ReleaseDate { get; set; }
+
+    public string? Genre { get; set; }
+
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal 18, 2")] // The database column is a decimal of 18 digits with 2 decimal places.
+    public decimal Price { get; set; }
 }
