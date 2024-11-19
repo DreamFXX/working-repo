ae8c4e2835e5928578b84e106f02e92ce82395ec
DreamFXX <filipek.husak@gmail.com>
2024-11-19 17:09:41 +0100

    .cs Movie writed

---
+++ BlazorWebAppMovies/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorWebAppMovies.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; } // ? -> Can hold null value.
+
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")] // type decimal with ,00 a zaoukr. na 18 čísel za , // [] vlastnosti Price typu.
+    public decimal Price { get; set; }
+
+}
