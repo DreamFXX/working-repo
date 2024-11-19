38ebc8dc7f6ba2a992e65a01f4b7ff653a556d93
DreamFXX <filipek.husak@gmail.com>
2024-11-19 17:40:23 +0100

    nuget error ?

--- BlazorWebAppMovies/Models/Movie.cs
+++ BlazorWebAppMovies/Models/Movie.cs
@@ -6,7 +6,7 @@ namespace BlazorWebAppMovies.Models;
 public class Movie
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public string? Title { get; set; }
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; } // ? -> Can hold null value.
 
