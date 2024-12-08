9f917b0f58bb39cd874ae50a6653c2d47e88ef47
DreamFXX <filipek.husak@gmail.com>
2024-12-08 19:50:55 +0100

    created last training project

---
+++ CRUD_training/CrudApplication.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using Spectre.Console;
+
+namespace CRUD_training;
+internal class CrudApplication
+{
+    private DatabaseManager dbManager = new DatabaseManager();
+
+    internal static void MainMenu()
+    {
+        
+    }
+}
+
---
+++ CRUD_training/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            CrudApplication.MainMenu();
+        }
+    }
+}
