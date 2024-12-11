78b0b831ab6ba32d3430de04a75c5986ff913b49
DreamFXX <filipek.husak@gmail.com>
2024-12-11 15:02:40 +0100

    codespaces upgrade repo

---
+++ HabitTracker_Advanced/2HabitTracker_Advanced/Models/Habit.cs
@@ -0,0 +1,9 @@
+﻿namespace _2HabitTracker_Advanced.Models;
+
+public class Habit
+{
+    public int Id { get; set; }
+    public DateTime DateAndTime { get; set; }
+    public int Quantity { get; set; }
+}
+
