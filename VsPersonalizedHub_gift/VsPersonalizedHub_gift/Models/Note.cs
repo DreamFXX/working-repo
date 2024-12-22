fa5ed12d81cfa053bef76ed23d02f0897dd92321
DreamFXX <filipek.husak@gmail.com>
2024-12-22 14:54:00 +0100

    created EFCore DBContext. not added to prg.cs

---
+++ VsPersonalizedHub_gift/VsPersonalizedHub_gift/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VsPersonalizedHub_gift.Models;
+
+public class Note
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Title { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; }
+}
